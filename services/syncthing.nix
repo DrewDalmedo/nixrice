c965931be1f50821a15298b5cd10abef8ef51ed9
Drew Dalmedo <andrewdalmedo@gmail.com>
2025-01-20 22:18:04 -0500

    Set up synced folders

--- services/syncthing.nix
+++ services/syncthing.nix
@@ -4,5 +4,18 @@
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
+
+    settings = {
+      folders = {
+        "Sync" = {
+          path = "/home/pengu/.local/sync";
+          # sync file permissions
+          ignorePerms = false; 
+        };
+      };  
+    };
   };
+
+  # disable default sync folder
+  systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true";
 }
