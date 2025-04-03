85c19ed4ab2fdfbd9db411e33ff614412bd6be16
Drew Dalmedo <andrewdalmedo@gmail.com>
2025-04-03 12:32:52 -0400

    Enable VirtualBox

--- hosts/voyager/configuration.nix
+++ hosts/voyager/configuration.nix
@@ -9,7 +9,7 @@
     [ 
       ./hardware-configuration.nix # Include the results of the hardware scan.
       #../../services/syncthing.nix
-      #./virtualbox.nix
+      ./virtualbox.nix
     ];
 
   programs.firefox.enable = false;
